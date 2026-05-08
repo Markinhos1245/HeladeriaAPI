340cb10fefc47efe55889bbc29a6be07b456ddb7
MarcosCateura <marcoscateura@gmail.com>
2026-05-08 19:28:21 -0300

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -73,7 +73,15 @@ def agregar_al_carrito():
     ---
     parameters:
       - in: body
-        name: producto
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            productoId:
+              type: integer
+            cantidad:
+              type: integer
 
     responses:
       200:
@@ -107,14 +115,24 @@ def agregar_al_carrito():
         "mensaje": "Producto agregado al carrito"
     })
 
+
 @app.route('/carrito/<int:producto_id>', methods=['DELETE'])
 def eliminar_del_carrito(producto_id):
     """
     Eliminar cantidad de producto del carrito
     ---
     parameters:
+      - name: producto_id
+        in: path
+        type: integer
+
       - in: body
-        name: producto
+        name: body
+        schema:
+          type: object
+          properties:
+            cantidad:
+              type: integer
 
     responses:
       200:
@@ -144,6 +162,7 @@ def eliminar_del_carrito(producto_id):
     return jsonify({
         "mensaje": "Cantidad eliminada correctamente"
     })
+
 
 @app.route('/carrito/total', methods=['GET'])
 def calcular_total():
